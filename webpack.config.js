3a7df5bcf7a8b41e66699609eae0ea3ab0eba362
Yohane <2540546236@qq.com>
2019-03-06 23:24:49 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 test: /\.(css|less)$/,
                 use: [
 
-                'style-loader',
+                'style-loader',   
                 'css-loader',
                 {
                         loader: 'postcss-loader',
